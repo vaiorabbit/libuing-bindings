8848e4c502fc22859c9f1f47e8ce221a9e90ea78
vaiorabbit <vaiorabbit>
2022-12-11 13:59:06 +0900

    No need to handle multiple library files

--- lib/libuing.rb
+++ lib/libuing.rb
@@ -10,21 +10,15 @@ module UI
 
   @@libuing_import_done = false
   def self.load_lib(libpath, output_error = false)
-
     unless @@libuing_import_done
-      # Ref.: Using Multiple and Alternate Libraries
-      # https://github.com/ffi/ffi/wiki/Using-Multiple-and-Alternate-Libraries
       begin
-        lib_paths = [libpath].compact
-
         ffi_lib_flags :now, :global
-        ffi_lib *lib_paths
+        ffi_lib libpath
         setup_symbols(output_error)
       rescue => error
-        $stderr.puts("[Warning] Failed to load libraries (#{error}).") if output_error
+        $stderr.puts("[Warning] Failed to load library (#{error}).") if output_error
       end
     end
-
   end
 
   def self.setup_symbols(output_error = false)
